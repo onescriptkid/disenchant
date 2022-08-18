4a40fc8ae7bf753118984e99e6f278a3c187da69
onescriptkid <onescriptkid@gmail.com>
2022-08-17 21:38:35 -0700

    Added loot parsing

--- README.md
+++ README.md
@@ -12,3 +12,51 @@ Disenchant all champion shards with 1 click
 - #️⃣ only works with windows
 - ~~🐧not compatible with linux~~
 - ~~🍎 not compatiable with OSX~~
+
+## Riot League Client API
+
+`disenchant.go` queries the riot league client [api](https://riot-api-libraries.readthedocs.io/en/latest/lcu.html) to gather loot and select champion shards to convert to blue essence
+
+Curl
+```
+curl --insecure --basic --user riot:<password> -H "Accept: application/json" -v https://localhost:65023/lo
+l-loot/v1/player-loot
+```
+
+```json
+[{
+	"asset":"",
+	"count":1,
+	"disenchantLootName":"CURRENCY_champion",
+	"disenchantValue":90,
+	"displayCategories":"CHAMPION",
+	"expiryTime":-1,
+	"isNew":false,
+	"isRental":true,
+	"itemDesc":"Ashe",
+	"itemStatus":"OWNED",
+	"localizedDescription":"",
+	"localizedName":"",
+	"localizedRecipeSubtitle":"",
+	"localizedRecipeTitle":"",
+	"lootId":"CHAMPION_RENTAL_22",
+	"lootName":"CHAMPION_RENTAL_22",
+	"parentItemStatus":"NONE",
+	"parentStoreItemId":-1,
+	"rarity":"DEFAULT",
+	"redeemableStatus":"ALREADY_OWNED",
+	"refId":"",
+	"rentalGames":0,
+	"rentalSeconds":604800,
+	"shadowPath":"",
+	"splashPath":"/lol-game-data/assets/v1/champion-splashes/22/22000.jpg",
+	"storeItemId":22,
+	"tags":"",
+	"tilePath":"/lol-game-data/assets/v1/champion-tiles/22/22000.jpg",
+	"type":"CHAMPION_RENTAL",
+	"upgradeEssenceName":"CURRENCY_champion",
+	"upgradeEssenceValue":270,
+	"upgradeLootName":"CHAMPION_22",
+	"value":450
+}]
+```
--- disenchant.go
+++ disenchant.go
@@ -9,9 +9,21 @@ import (
 	"path/filepath"
 	"strings"
 	"encoding/base64"
+	"net/http"
+	"time"
+	"crypto/tls"
+	"encoding/json"
 	// "os"
 	// "net/http"
 )
+
+type RiotLoot struct {
+	DisenchantLootName string `json:"disenchantLootName"`
+	ItemStatus string `json:"itemStatus"`
+	ItemDesc string `json:"itemDesc"`
+	LootName string `json:"lootName"`
+	Count int `json:"count"`
+}
 
 func main() {
 	utils.Title("Disenchanting blue essence ...")
@@ -24,13 +36,13 @@ func main() {
 	}
 	
 	// Build http client to interact with Riot Lol client api
-	BuildHttpClient(port, token)
+	client, err := BuildHttpClient(port, token)
 	if err != nil {
 		utils.ErrorFatal(err)
 	}
 
 	// List all champion shards convertable to blue essence
-	// listChampionShards()
+	listChampionShards()
 
 	// Prompt player before disenchanting all of their champion shards - Are you sure?
 	// areYouSure()
@@ -113,13 +125,62 @@ func getPortAndToken() ( port string, token string, err error ) {
 }
 
 // Build http client to interact with Riot Lol client api
-func BuildHttpClient(port string, token string)(err error) {
+func BuildHttpClient(port string, token string)(client http.Client, err error) {
 	utils.Header("Building http client ...")
+	host := fmt.Sprintf("https://127.0.0.1:%s", port)
+	auth := fmt.Sprintf("Basic %s", token)
+	url := fmt.Sprintf("%s/lol-loot/v1/player-loot", host)
+
+	// Instantiate http client
+	tr := &http.Transport{ TLSClientConfig: &tls.Config{InsecureSkipVerify: true} }
+	client = http.Client{Timeout: time.Duration(1) * time.Second, Transport: tr}
+
+	// Instantiate http get request
+	req, httperr := http.NewRequest("GET", url, nil)
+	if httperr != nil {
+		err = httperr
+		return
+	}
+
+	// Set headers on RiotLoot get request
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", auth)
+
+	// Execute RiotLoot get request
+	utils.Header("Searching for champions to disenchant ...")
+	res, geterr := client.Do(req)
+	if geterr != nil {
+		err = geterr
+		return
+	}
+
+	// Check status code
+	if res.StatusCode != http.StatusOK {
+		msg := fmt.Sprintf("Request to %s failed with status %v %v", url, res.StatusCode, http.StatusText(res.StatusCode))
+		err = errors.New(msg)
+		return
+	}
+
+	// Unmarshall RiotLoot get request into json
+	var riotLoot []RiotLoot
+	json.NewDecoder(res.Body).Decode(&riotLoot)
+	// fmt.Println("Decoded Json")
+	// fmt.Println(riotLoot)
+
+	// Iterate over all RiotLoot and only select champions that are owned
+	for _, loot := range riotLoot {
+		if loot.DisenchantLootName == "CURRENCY_champion" && loot.ItemStatus == "OWNED" {
+			fmt.Printf("  Found %4v %s \n", loot.Count, loot.ItemDesc,)
+		}
+	}
+
 	return
 }
 
 // List all champion shards convertable to blue essence
-// func listChampionShards() {}
+func listChampionShards(client http.Client, port string, token string)(err error) {
+	utils.Header("Searching for champions to disenchant ...")
+}
 
 // Prompt player before disenchanting all of their champion shards - Are you sure?
 // func areYouSure() {}
